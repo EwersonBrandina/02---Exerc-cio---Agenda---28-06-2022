accacf293c4be9d152968a0d1e49439b637304ce
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-06-29 11:14:18 -0300

    Atualização - 20/06/2022 - 11:14

--- class_menu.py
+++ class_menu.py
@@ -1,13 +1,16 @@
 from class_estoque import*
 from class_compras import*
+from class_vendas import*
 
 class Menu():
     def __init__(self):
         estoque = Estoque()
         compra = Compras()
         compra.entrada = estoque
+        venda = Vendas()
+        venda.entrada = estoque
         while True:
-            entrada=input(('1 - Novo Cadastro\n2 - Listar Produto\n3 - Alterar Prod..: \n4 - Comprar: \n0 - Sair\n: '))
+            entrada=input(('1 - Novo Cadastro\n2 - Listar Produto\n3 - Alterar Prod..: \n4 - Comprar: \n5 - Vender\n0 - Sair\n: '))
             if entrada == '1':
                 estoque.salvar_produtos()
                 pass
@@ -17,10 +20,12 @@ class Menu():
             elif entrada == '3':
                 estoque.alterar_produtos()
                 pass
+            elif entrada == '4':
+                compra.comprar()
+            elif entrada == '5':
+                venda.vender()
             elif entrada == '0':
                 print('Obrigado por acessar. Volte Sempre.')
                 break
-            elif entrada == '4':
-                compra.comprar()
             else:
                 print('Opção Invalida!!!')
---
+++ class_vendas.py
@@ -0,0 +1,11 @@
+from class_estoque import*
+
+class Vendas:
+    def __init__(self):
+        self.entrada = Estoque()
+    def vender(self):
+        entrada = input('Cod do Produto:  ')
+        for i in range(len(self.entrada.listaProdutos)):
+            if entrada == self.entrada.listaProdutos[i].cod:
+                self.entrada.listaProdutos[i].quantidade -= int(input('Quantidade vendida:  '))
+
