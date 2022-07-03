f001276a444682f98bbf5750e8d34f75ae7a3881
EwersonBrandina <106076391+EwersonBrandina@users.noreply.github.com>
2022-07-03 19:31:02 -0300

    Update class_vendas.py

--- class_vendas.py
+++ class_vendas.py
@@ -1,4 +1,4 @@
-from class_estoque import* 
+from class_estoque import*
 from class_historico import*
 
 class Vendas:
@@ -11,6 +11,6 @@ class Vendas:
             if entrada == self.entrada.listaProdutos[i].cod:
                 x=int(input('Quantidade vendida:  '))
                 self.entrada.listaProdutos[i].quantidade -= int(x)
-                self.historico.transacoes.append(f'Venda de {x} unidades')
+                self.historico.transacoes.append(f'Venda de {x} unidades do produto:', self.entrada.listaProdutos[i].nome)
     def extrato(self):
         print(self.historico.compras_vendas())
