2f131bfb67c4a0abc2888df5da9e9c5b28dfe8bb
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-07-07 11:20:33 -0300

    Atualização  - 07/07/2022 - 11:20

--- class_estoque_db.py
+++ class_estoque_db.py
@@ -15,9 +15,21 @@ class Estoque:
         self.meu_cursor = self.conexao.cursor()
         
     #Create
-    def salvar_produtos(self, cod, nome, fabricante):
-        obj_produto = Produto(cod, nome, fabricante)
-        comando_sql = f'insert into Produtos (nome, fabricante) value ("{obj_produto.nome}","{obj_produto.fabricante}")'
+    def salvar_fabricantes(self, cod, nome):
+        obj_fabricante = Fabricante(cod, nome)
+        comando_sql = f'insert into Fabricantes (nome) value ("{obj_fabricante.nome}")'
+        self.meu_cursor.execute(comando_sql)
+        self.conexao.commit()
+        #self.listaFabricantes.append(Fabricante('1-' + str(len(self.listaFabricantes) + 1), input('Nome: ')))
+
+    def salvar_produtos(self, cod, nome, fabricante, quantidade):
+        obj_produto = Produto(cod, nome, fabricante, quantidade)
+
+        escolha = f'select nome from Fabricantes where id = {obj_produto.fabricante}'
+        self.meu_cursor.execute(escolha)
+        lista = self.meu_cursor.fetchall()
+        
+        comando_sql = f'insert into Produtos (nome, fabricante, quantidade) value ("{obj_produto.nome}","{lista[0]}",{obj_produto.quantidade})'
         self.meu_cursor.execute(comando_sql)
         self.conexao.commit()
 
@@ -33,21 +45,21 @@ class Estoque:
         #else:
         #    print('Não há Fabricante cadastrado. Cadastre-o primeiro')
 
-    def salvar_fabricantes(self, cod, nome):
-        obj_fabricante = Fabricante(cod, nome)
-        comando_sql = f'insert into Fabricantes (nome) value ("{obj_fabricante.nome}")'
-        self.meu_cursor.execute(comando_sql)
-        self.conexao.commit()
-        #self.listaFabricantes.append(Fabricante('1-' + str(len(self.listaFabricantes) + 1), input('Nome: ')))
-
     #Read
-    def listar_fabricantes(self, tabela):
+    def listar(self, tabela):
         comando_sql = f'select * from {tabela}'
         self.meu_cursor.execute(comando_sql)
         lista = self.meu_cursor.fetchall()
         for i in lista:
             print(i)
         #self.mostrar_fabricantes()
+
+    def listar_tabelas (self):
+        comando_sql = f'show tables;'
+        self.meu_cursor.execute(comando_sql)
+        lista = self.meu_cursor.fetchall()
+        for i in lista:
+            print(i)
 
     #def listar_produtos(self):
     #    comando_sql = f'select * from Produtos'
