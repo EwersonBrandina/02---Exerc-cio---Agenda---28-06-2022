40870d60db30ba57c54bf66a8020960dd55b044a
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-07-07 08:33:27 -0300

    Atualização - 07/07/2022 - 08:33

--- class_estoque_db.py
+++ class_estoque_db.py
@@ -13,12 +13,14 @@ class Estoque:
             database='estoque'
         )
         self.meu_cursor = self.conexao.cursor()
+        
     #Create
     def salvar_produtos(self, cod, nome, fabricante):
         obj_produto = Produto(cod, nome, fabricante)
-        comando_sql = f'insert into Produtos (nome, fabricante) value ({obj_produto.nome},{obj_produto.fabricante})'
+        comando_sql = f'insert into Produtos (nome, fabricante) value ("{obj_produto.nome}","{obj_produto.fabricante}")'
         self.meu_cursor.execute(comando_sql)
         self.conexao.commit()
+
         #if len(self.listaFabricantes) > 0:
         #    self.mostrar_fabricantes()
         #    x = input('Insira o código do fabricante de seu produto.\n: ')
@@ -31,29 +33,28 @@ class Estoque:
         #else:
         #    print('Não há Fabricante cadastrado. Cadastre-o primeiro')
 
-    def salvar_fabricantes(self, cod, nome_frabricante):
-        obj_fabricante = Fabricante(cod, nome_frabricante)
-        comando_sql = f'insert into Fabricantes (nome_fabricantes) value ({obj_fabricante.nome_frabricante})'
+    def salvar_fabricantes(self, cod, nome):
+        obj_fabricante = Fabricante(cod, nome)
+        comando_sql = f'insert into Fabricantes (nome) value ("{obj_fabricante.nome}")'
         self.meu_cursor.execute(comando_sql)
         self.conexao.commit()
         #self.listaFabricantes.append(Fabricante('1-' + str(len(self.listaFabricantes) + 1), input('Nome: ')))
 
     #Read
-    def listar_fabricantes(self):
-        comando_sql = f'select * from Fabricantes'
+    def listar_fabricantes(self, tabela):
+        comando_sql = f'select * from {tabela}'
         self.meu_cursor.execute(comando_sql)
         lista = self.meu_cursor.fetchall()
         for i in lista:
             print(i)
         #self.mostrar_fabricantes()
 
-    #Read
-    def listar_produtos(self):
-        comando_sql = f'select * from Produtos'
-        self.meu_cursor.execute(comando_sql)
-        lista = self.meu_cursor.fetchall()
-        for i in lista:
-            print(i)
+    #def listar_produtos(self):
+    #    comando_sql = f'select * from Produtos'
+    #    self.meu_cursor.execute(comando_sql)
+    #    lista = self.meu_cursor.fetchall()
+    #    for i in lista:
+    #        print(i)
 
         #x = input('1 - Todos.\n2 - Por Cod.\n3 - Por Nome.\n')
         #if x == '1':
@@ -95,7 +96,6 @@ class Estoque:
         #        self.listaFabricantes[i].nome_frabricante = input('Insira aqui o novo nome.\n: ')
         #        print(self.listaFabricantes[i].nome_frabricante)
 
-    #Update
     #def alterar_produtos(self, tabela, valor, cod):
         #self.mostrar_produtos()
         #in_cod = input('Insira o código do produto.\n: ')
@@ -104,40 +104,45 @@ class Estoque:
         #        self.listaProdutos[i].nome = input('Insira aqui o novo nome.\n: ')
         #        print(self.listaProdutos[i].nome)
 
-    def excluir_fabricantes(self):
-        self.mostrar_fabricantes()
-        in_cod = input('Insira o código do produto.\n: ')
-        for i in range(len(self.listaFabricantes)):
-            if self.listaFabricantes[i].cod == in_cod:
-                self.listaFabricantes.pop(i)
-                print('Fabricante excluído com sucesso!!!')
-                self.mostrar_fabricantes()
-                break
-            else:
-                pass
+    #Delete
+    def excluir(self, tabela, cod):
+        comando_sql = f'delete from {tabela} where id = {cod}'
+        self.meu_cursor.execute(comando_sql)
+        self.conexao.commit()
 
-    def excluir_produtos(self):
-        self.mostrar_produtos()
-        in_cod = input('Insira o código do produto.\n: ')
-        for i in range(len(self.listaProdutos)):
-            if self.listaProdutos[i].cod == in_cod:
-                self.listaProdutos.pop(i)
-                print('Produto excluido com sucesso!!!')
-                self.mostrar_produtos()
-                break
-            else:
-                pass
+        #self.mostrar_fabricantes()
+        #in_cod = input('Insira o código do produto.\n: ')
+        #for i in range(len(self.listaFabricantes)):
+        #    if self.listaFabricantes[i].cod == in_cod:
+        #        self.listaFabricantes.pop(i)
+        #        print('Fabricante excluído com sucesso!!!')
+        #        self.mostrar_fabricantes()
+        #        break
+        #    else:
+        #        pass
 
-    def mostrar_produtos(self):
-        print('Produtos')
-        for i in range(len(self.listaProdutos)):
-            print('Código: ', self.listaProdutos[i].cod,
-                  'Nome: ', self.listaProdutos[i].nome,
-                  'Fabricante: ', self.listaProdutos[i].fabricante,
-                  'Quantidade: ', self.listaProdutos[i].quantidade)
+    #def excluir_produtos(self):
+    #    self.mostrar_produtos()
+    #    in_cod = input('Insira o código do produto.\n: ')
+    #    for i in range(len(self.listaProdutos)):
+    #        if self.listaProdutos[i].cod == in_cod:
+    #            self.listaProdutos.pop(i)
+    #            print('Produto excluido com sucesso!!!')
+    #            self.mostrar_produtos()
+    #            break
+    #        else:
+    #            pass
 
-    def mostrar_fabricantes(self):
-        print('Fabricantes')
-        for i in range(len(self.listaFabricantes)):
-            print('Código: ', self.listaFabricantes[i].cod,
-                  'Nome: ', self.listaFabricantes[i].nome_frabricante)
+    #def mostrar_produtos(self):
+    #    print('Produtos')
+    #    for i in range(len(self.listaProdutos)):
+    #        print('Código: ', self.listaProdutos[i].cod,
+    #              'Nome: ', self.listaProdutos[i].nome,
+    #              'Fabricante: ', self.listaProdutos[i].fabricante,
+    #              'Quantidade: ', self.listaProdutos[i].quantidade)
+
+    #def mostrar_fabricantes(self):
+    #    print('Fabricantes')
+    #    for i in range(len(self.listaFabricantes)):
+    #       print('Código: ', self.listaFabricantes[i].cod,
+    #              'Nome: ', self.listaFabricantes[i].nome_frabricante)
