68e7f34923e136f02b4ec4644f566a3f6d040a75
EwersonBrandina <ewerson.brandina@hotmail.com.br>
2022-07-13 19:08:38 -0300

    Atualização - 13/07/2022 - 10:09

--- class_estoque_db.py
+++ class_estoque_db.py
@@ -3,11 +3,12 @@ from class_fabricante import Fabricante
 from class_produto import Produto
 
 class Estoque:
-    def __init__(self): 
+    def __init__(self, msg='Segue Lista Abaixo:'): 
+        self.msg = msg
         self.conexao = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='q1w2e3',
+            password='92337155Thule03@1',
             database='estoque'
         )
         self.meu_cursor = self.conexao.cursor() 
@@ -25,18 +26,31 @@ class Estoque:
         self.conexao.commit()
     #Read
     def listar(self, tabela):
-        comando_sql = f'select * from {tabela}'
-        self.meu_cursor.execute(comando_sql)
-        lista = self.meu_cursor.fetchall()
-        for i in lista:
-            print(i)
+        self.msg = 'Segue Lista Abaixo:'
+        if tabela != 'Produtos' and tabela != 'Fabricantes':
+            print('Tabela Inexistente.')
+            self.msg += f'Tabela Inexistente'
+        else:
+            comando_sql = f'select * from {tabela}'
+            self.meu_cursor.execute(comando_sql)
+            lista = self.meu_cursor.fetchall()
+            if tabela == 'produtos' or tabela == 'Produtos':
+                for i in lista:
+                    print(i[0],i[1],i[2],i[3])
+                    self.msg += f'\nCod: {i[0]}, Nome: {i[1]}, Fabr.: {i[2]}, Quant.:{i[3]}'
+            elif tabela == 'fabricantes' or tabela == 'Fabricantes':
+                for i in lista:
+                    print(i[0])
+                    self.msg += f'\nCod: {i[0]}, Fabr.: {i[1]}'
     #Read All Tables
     def listar_tabelas (self):
+        self.msg = 'Segue Lista Abaixo:'
         comando_sql = f'show tables;'
         self.meu_cursor.execute(comando_sql)
         lista = self.meu_cursor.fetchall()
         for i in lista:
-            print(i)
+            print(i[0])
+            self.msg += f'\n Tabela: {i[0].title()}'
     #Update
     def alterar_tabela(self, tabela, atributo, valor, cod):
         comando_sql = f'update {tabela} set {atributo} = "{valor}" where id = {cod}'
